7704ef3f77c3f220ca2db846126c936c3387e69c
Victoria Dezerega <videzere@gmail.com>
2021-06-13 23:14:36 -0400

    Actualizar GitHud v1 from VS

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,61 @@
 import React from "react";
+import { Container, Form, FormGroup, ListGroup } from "react-bootstrap";
+import { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 export function Home() {
+	const [tasks, setTasks] = useState([]);
+	const [addtask, setAdd] = useState("");
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		let backup = tasks;
+		setTasks([...backup, addtask]);
+		setAdd("");
+	};
+
+	const deletetask = index => {
+		setTasks(tasks.filter((task, i) => i !== index));
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<Container className="container bg-light d-flex flex-column justify-content-center text-center pt-5">
+			<h1>Todos</h1>
+			<Form onSubmit={e => handleSubmit(e)}>
+				<Form.Group controlId="formTodo">
+					<Form.Control
+						type="text"
+						placeholder="What needs to be done?"
+						value={addtask}
+						onChange={e => setAdd(e.target.value)}
+					/>
+				</Form.Group>
+			</Form>
+			<ListGroup>
+				{tasks.length === 0 ? (
+					<ListGroup.Item className="d-flex justify-content-left text-secondary p-2">
+						Empty
+					</ListGroup.Item>
+				) : (
+					tasks.map((tasks, index) => {
+						return (
+							<ListGroup.Item
+								key={index}
+								className="d-flex justify-content-between text-secondary p-2 show">
+								{tasks}
+								{""}
+								<span onClick={e => deletetask(index)}>✖</span>
+							</ListGroup.Item>
+						);
+					})
+				)}
+			</ListGroup>
+			<div className="d-flex justify-content-left text-secondary pt-4 pl-2 pb-4">
+				{tasks.length === 1
+					? `${tasks.length} item left`
+					: tasks.length == 0
+					? "0 items"
+					: `${tasks.length} items left`}
+			</div>
+		</Container>
 	);
 }
